a9122f6e2f05426c8334ebbd315aeb577003f4cf
Alexander Saul <alexandersaul2604@gmail.com>
2025-11-22 17:07:46 -0500

    first commit

---
+++ src/main/java/com/alexander/fullday/FulldayApplication.java
@@ -0,0 +1,13 @@
+package com.alexander.fullday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulldayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulldayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexander/fullday/FulldayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexander.fullday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulldayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
