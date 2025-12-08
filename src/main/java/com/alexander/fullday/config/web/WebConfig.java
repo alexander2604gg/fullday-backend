d535f3b1d64bc339c919cb122900d5c85a17f615
Alexander Saul <alexandersaul2604@gmail.com>
2025-12-08 14:03:30 -0500

    feat: config

--- src/main/java/com/alexander/fullday/config/web/WebConfig.java
+++ src/main/java/com/alexander/fullday/config/web/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Todas las rutas
-                .allowedOrigins("https://landingfulldayunt2025.vercel.app") // Solo tu front
+                .allowedOrigins("https://fullday2025unt.vercel.app") // Solo tu front
                 .allowedMethods("*") // Todos los métodos permitidos (GET, POST, etc.)
                 .allowedHeaders("*") // Todos los headers permitidos
                 .allowCredentials(true); // Si necesitas enviar cookies o headers de autenticación
